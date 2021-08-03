c6658b51050b4c495a4455a614637dbd0f935f2c
Arnold Wang <rea-yingwang@Yings-MacBook-Pro.local>
2021-08-03 22:19:43 +0800

    feat: add profile image.

--- app/src/main/java/com/example/jetpackcomposemoment/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposemoment/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,8 +20,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -75,8 +79,23 @@ fun MessageCard(msg: Message) {
 }
 
 @Composable
+fun ProfileCard(userName: String) {
+    Image(
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.FillWidth,
+        alignment = Alignment.TopStart,
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentDescription = "User profile image."
+    )
+    Row(horizontalArrangement = Arrangement.End) {
+        Text(text = userName)
+    }
+}
+
+@Composable
 fun TweetList(messages: List<Message>) {
     LazyColumn {
+        item { ProfileCard(userName = "Ying Wang") }
         items(messages) { message ->
             MessageCard(msg = message)
         }
